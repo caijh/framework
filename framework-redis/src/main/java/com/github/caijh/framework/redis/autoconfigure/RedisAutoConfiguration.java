ef53fe7ed6599e9cdc60aa3439f3d7e17097a0f9
caijunhui <caijunhui@pcitech.com>
2020-08-12 17:49:13 +0800

    feat: add framework-redis

---
+++ framework-redis/src/main/java/com/github/caijh/framework/redis/autoconfigure/RedisAutoConfiguration.java
@@ -0,0 +1,28 @@
+package com.github.caijh.framework.redis.autoconfigure;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@ConditionalOnMissingBean(value = {RedisConnectionFactory.class, RedisTemplate.class})
+public class RedisAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setEnableTransactionSupport(true);
+        return redisTemplate;
+    }
+
+}
