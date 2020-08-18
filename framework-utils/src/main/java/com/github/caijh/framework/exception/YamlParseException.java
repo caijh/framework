ede02fe53ad1f02847920a2a092692c886cfe01d
caijunhui <caijunhui@pcitech.com>
2020-08-18 09:11:41 +0800

    feat: add framework-utils

---
+++ framework-utils/src/main/java/com/github/caijh/framework/exception/YamlParseException.java
@@ -0,0 +1,9 @@
+package com.github.caijh.framework.exception;
+
+public class YamlParseException extends RuntimeException {
+
+    public YamlParseException(Exception e) {
+        super(e);
+    }
+
+}
