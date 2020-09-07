d14bc24ff3a1bd7561c8bc62257a2b61c0dfd84f
caijunhui <caijunhui@pcitech.com>
2020-09-07 16:12:53 +0800

    feat: add mybatis

---
+++ framework-mybatis/src/main/java/com/github/caijh/framework/data/mybatis/autoconfigure/FrameworkMybatisAutoConfiguration.java
@@ -0,0 +1,8 @@
+package com.github.caijh.framework.data.mybatis.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FrameworkMybatisAutoConfiguration {
+
+}
---
+++ framework-mybatis/src/main/java/com/github/caijh/framework/data/mybatis/autoconfigure/processor/StarterConfigFileEnvironmentPostProcessor.java
@@ -0,0 +1,7 @@
+package com.github.caijh.framework.data.mybatis.autoconfigure.processor;
+
+import com.github.caijh.framework.core.processor.ConfigFileEnvironmentPostProcessor;
+
+public class StarterConfigFileEnvironmentPostProcessor extends ConfigFileEnvironmentPostProcessor {
+
+}
