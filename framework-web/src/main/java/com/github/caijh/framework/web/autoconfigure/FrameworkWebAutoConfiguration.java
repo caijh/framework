0adb083ca41e6d7bfcb33ac0bb776041c867d662
蔡俊辉 <caqizhe@gmail.com>
2020-09-12 14:45:25 +0800

    feat: add smart-doc

--- framework-web/src/main/java/com/github/caijh/framework/web/autoconfigure/FrameworkWebAutoConfiguration.java
+++ framework-web/src/main/java/com/github/caijh/framework/web/autoconfigure/FrameworkWebAutoConfiguration.java
@@ -1,10 +1,17 @@
 package com.github.caijh.framework.web.autoconfigure;
 
+import com.github.caijh.framework.web.init.DocInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class FrameworkWebAutoConfiguration {
+
+    @Bean
+    public DocInitializer docInitializer() {
+        return new DocInitializer();
+    }
 
 }
---
+++ framework-web/src/main/java/com/github/caijh/framework/web/init/DocInitializer.java
@@ -0,0 +1,45 @@
+package com.github.caijh.framework.web.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.power.common.enums.HttpCodeEnum;
+import com.power.doc.builder.HtmlApiDocBuilder;
+import com.power.doc.model.ApiConfig;
+import com.power.doc.model.ApiErrorCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class DocInitializer implements ApplicationListener<ApplicationStartedEvent> {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void onApplicationEvent(ApplicationStartedEvent event) {
+        logger.info("Initializing smart-doc");
+        init(event.getApplicationContext());
+    }
+
+    public void init(ConfigurableApplicationContext applicationContext) {
+        ApiConfig config = new ApiConfig();
+        config.setAllInOne(true);
+        config.setCoverOld(true);
+        config.setAdoc(true);
+        config.setStrict(true);
+        config.setProjectName(applicationContext.getApplicationName());
+        config.setServerUrl("http://127.0.0.1:" + applicationContext.getEnvironment().getProperty("server.port"));
+        config.setOutPath("doc/api");
+        List<ApiErrorCode> errorCodeList = new ArrayList<>();
+        for (HttpCodeEnum codeEnum : HttpCodeEnum.values()) {
+            ApiErrorCode errorCode = new ApiErrorCode();
+            errorCode.setValue(codeEnum.getCode()).setDesc(codeEnum.getMessage());
+            errorCodeList.add(errorCode);
+        }
+        config.setErrorCodes(errorCodeList);
+        HtmlApiDocBuilder.buildApiDoc(config);
+    }
+
+}
