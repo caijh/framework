c1dcf2e7eb63735a32bd72d1d831e05e0f9e01f8
caijunhui <caijunhui@pcitech.com>
2020-09-28 20:08:00 +0800

    refactor: THREAD_LOCAL_STORE to STORE

--- framework-web/src/main/java/com/github/caijh/framework/web/threadlocal/ThreadLocalStore.java
+++ framework-web/src/main/java/com/github/caijh/framework/web/threadlocal/ThreadLocalStore.java
@@ -11,7 +11,7 @@ public class ThreadLocalStore {
 
     }
 
-    private static final ThreadLocal<Map<Object, Object>> THREAD_LOCAL_STORE = new NamedThreadLocal<Map<Object, Object>>("thread local store") {
+    private static final ThreadLocal<Map<Object, Object>> STORE = new NamedThreadLocal<Map<Object, Object>>("thread local store") {
         @Override
         protected Map<Object, Object> initialValue() {
             return new HashMap<>();
@@ -19,12 +19,12 @@ public class ThreadLocalStore {
     };
 
     public static void put(Object key, Object value) {
-        THREAD_LOCAL_STORE.get().put(key, value);
+        STORE.get().put(key, value);
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T get(Object key) {
-        Object result = THREAD_LOCAL_STORE.get().get(key);
+        Object result = STORE.get().get(key);
 
         if (result == null) {
             return null;
@@ -33,11 +33,11 @@ public class ThreadLocalStore {
     }
 
     public static void reset() {
-        THREAD_LOCAL_STORE.remove();
+        STORE.remove();
     }
 
     public void remove(Object key) {
-        THREAD_LOCAL_STORE.get().remove(key);
+        STORE.get().remove(key);
     }
 
 }
