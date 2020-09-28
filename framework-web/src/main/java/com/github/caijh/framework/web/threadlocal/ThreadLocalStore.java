65928fd1ba05130e24a6b84f921fdfdb542af534
caijunhui <caijunhui@pcitech.com>
2020-09-28 19:59:20 +0800

    fix: invoke put fail

--- framework-web/src/main/java/com/github/caijh/framework/web/threadlocal/ThreadLocalStore.java
+++ framework-web/src/main/java/com/github/caijh/framework/web/threadlocal/ThreadLocalStore.java
@@ -1,24 +1,30 @@
 package com.github.caijh.framework.web.threadlocal;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.core.NamedThreadLocal;
 
 public class ThreadLocalStore {
 
-    private static final ThreadLocal<Map<Object, Object>> STORE = new NamedThreadLocal<>("thread local store");
-
     private ThreadLocalStore() {
 
     }
 
+    private static final ThreadLocal<Map<Object, Object>> THREAD_LOCAL_STORE = new NamedThreadLocal<Map<Object, Object>>("thread local store") {
+        @Override
+        protected Map<Object, Object> initialValue() {
+            return new HashMap<>();
+        }
+    };
+
     public static void put(Object key, Object value) {
-        STORE.get().put(key, value);
+        THREAD_LOCAL_STORE.get().put(key, value);
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T get(Object key) {
-        Object result = STORE.get().get(key);
+        Object result = THREAD_LOCAL_STORE.get().get(key);
 
         if (result == null) {
             return null;
@@ -27,11 +33,11 @@ public class ThreadLocalStore {
     }
 
     public static void reset() {
-        STORE.remove();
+        THREAD_LOCAL_STORE.remove();
     }
 
     public void remove(Object key) {
-        STORE.get().remove(key);
+        THREAD_LOCAL_STORE.get().remove(key);
     }
 
 }
