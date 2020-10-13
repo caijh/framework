62543181fbf0383ef776a23358d2a62ec628166d
caijunhui <caiqizhe@gmail.com>
2020-10-13 16:33:11 +0800

    feat: add ThreadLocalStoreFilter to reset shared thread store,ThreadLocalStoreInterceptor to clean after handle done
    maybe just add onely one is ok, but i add both.

--- framework-web/src/main/java/com/github/caijh/framework/web/interceptor/ThreadLocalStoreInterceptor.java
+++ framework-web/src/main/java/com/github/caijh/framework/web/interceptor/ThreadLocalStoreInterceptor.java
@@ -4,12 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.github.caijh.framework.web.threadlocal.ThreadLocalStore;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class ThreadLocalStoreInterceptor extends HandlerInterceptorAdapter {
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    public void afterCompletion(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler, Exception ex) throws Exception {
         super.afterCompletion(request, response, handler, ex);
         ThreadLocalStore.reset();
     }
