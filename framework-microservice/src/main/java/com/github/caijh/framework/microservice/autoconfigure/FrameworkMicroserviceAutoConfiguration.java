3443bc3bbdcfa078ce07c80489fa719f8fcee593
caijunhui <caiqizhe@gmail.com>
2020-10-20 17:26:24 +0800

    feat(framework-microservice): add openfeign,hystrix
    EnableFeignClients 还是需要在项目中手动开启

--- framework-microservice/src/main/java/com/github/caijh/framework/microservice/autoconfigure/FrameworkMicroserviceAutoConfiguration.java
+++ framework-microservice/src/main/java/com/github/caijh/framework/microservice/autoconfigure/FrameworkMicroserviceAutoConfiguration.java
@@ -1,10 +1,37 @@
 package com.github.caijh.framework.microservice.autoconfigure;
 
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import feign.Contract;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.HystrixCircuitBreakerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableDiscoveryClient
+//@EnableFeignClients
+@EnableHystrix
 public class FrameworkMicroserviceAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Contract feignContract() {
+        return new feign.Contract.Default();
+    }
+
+    @Bean
+    public Customizer<HystrixCircuitBreakerFactory> defaultConfig() {
+        return factory -> factory.configureDefault(id -> HystrixCommand.Setter
+                .withGroupKey(HystrixCommandGroupKey.Factory.asKey(id))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                                                                      .withExecutionTimeoutInMilliseconds(5000)
+                                                                      .withExecutionIsolationStrategy(HystrixCommandProperties.ExecutionIsolationStrategy.THREAD))
+        );
+    }
 
 }
