d9cd4d6030daeef2c94c157d65d85e1695b8791f
caijunhui <caiqizhe@gmail.com>
2020-10-27 17:29:20 +0800

    refactor

--- framework-microservice/src/main/java/com/github/caijh/framework/microservice/autoconfigure/FrameworkMicroserviceAutoConfiguration.java
+++ framework-microservice/src/main/java/com/github/caijh/framework/microservice/autoconfigure/FrameworkMicroserviceAutoConfiguration.java
@@ -7,12 +7,13 @@ import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.HystrixCircuitBreakerFactory;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableDiscoveryClient
-//@EnableFeignClients
+@EnableFeignClients
 @EnableHystrix
 public class FrameworkMicroserviceAutoConfiguration {
 
