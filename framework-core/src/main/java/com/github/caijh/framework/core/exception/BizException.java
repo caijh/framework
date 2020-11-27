4da6bbe0cfd18be0cb1d5bc46602726d71049275
caijh <caiqizhe@gmail.com>
2020-11-27 11:42:52 +0800

    feat: 异常类中增加国际化code

--- framework-core/src/main/java/com/github/caijh/framework/core/exception/BizException.java
+++ framework-core/src/main/java/com/github/caijh/framework/core/exception/BizException.java
@@ -1,26 +1,26 @@
 package com.github.caijh.framework.core.exception;
 
-import java.io.Serializable;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class BizException extends RuntimeException implements Serializable {
+public class BizException extends RuntimeException {
 
     /**
      * 业务异常代码，同时作为异常消息国际化的Key.
      */
-    private String code;
+    private final String code;
 
     /**
      * 国际化参数.
      */
-    private Object[] params;
+    private final transient Object[] params;
 
     public BizException() {
         super();
+        this.code = null;
+        this.params = null;
     }
 
     public BizException(String code, Object... params) {
@@ -31,14 +31,20 @@ public class BizException extends RuntimeException implements Serializable {
 
     public BizException(Exception e) {
         super(e);
+        this.code = null;
+        this.params = null;
     }
 
     public BizException(String message) {
         super(message);
+        this.code = null;
+        this.params = null;
     }
 
     public BizException(String message, Throwable cause) {
         super(message, cause);
+        this.code = null;
+        this.params = null;
     }
 
 }
