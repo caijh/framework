256c07dc38aa4e03b7c498ac287283e8f6dbb92f
caijh <caiqizhe@gmail.com>
2020-12-08 19:47:43 +0800

    refactor: 只扫描注解了@Api并且@ApiOperation，或者@Endpoint
    1. 从BuildProperties获取版本信息

--- framework-doc/src/main/java/com/github/caijh/framework/doc/autoconfigure/FrameworkDocAutoConfig.java
+++ framework-doc/src/main/java/com/github/caijh/framework/doc/autoconfigure/FrameworkDocAutoConfig.java
@@ -3,15 +3,18 @@ package com.github.caijh.framework.doc.autoconfigure;
 import com.github.caijh.framework.doc.autoconfigure.properties.SmartDocProperties;
 import com.github.caijh.framework.doc.autoconfigure.properties.SwaggerProperties;
 import com.github.caijh.framework.doc.init.DocInitializer;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -31,25 +34,34 @@ import springfox.documentation.spring.web.plugins.Docket;
 @AutoConfigureAfter(WebMvcAutoConfiguration.class)
 public class FrameworkDocAutoConfig {
 
-    @ConditionalOnBean(SwaggerProperties.class)
-    @ConditionalOnProperty(prefix = SwaggerProperties.PREFIX, value = "enabled")
+    @Autowired(required = false)
+    private BuildProperties buildProperties;
+
     @Bean
+    @ConditionalOnProperty(prefix = SwaggerProperties.PREFIX, value = "enabled")
+    @ConditionalOnMissingBean
     public Docket createRestApi(SwaggerProperties swaggerProperties) {
         return new Docket(DocumentationType.OAS_30).apiInfo(apiInfo(swaggerProperties))
                                                    // 是否开启
                                                    .enable(swaggerProperties.isEnabled()).select()
-                                                   .apis(RequestHandlerSelectors.withClassAnnotation(Controller.class)
-                                                                                .and(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)))
+                                                   // 只扫描注解了@Api并且@ApiOperation，或者@Endpoint
+                                                   .apis(RequestHandlerSelectors.withClassAnnotation(Api.class)
+                                                                                .and(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                                                                                .or(RequestHandlerSelectors.withClassAnnotation(Endpoint.class)))
                                                    // 指定路径处理PathSelectors.any()代表所有的路径
                                                    .paths(PathSelectors.any()).build();
     }
 
     private ApiInfo apiInfo(SwaggerProperties swaggerProperties) {
+        String version = "1.0.0";
+        if (buildProperties != null) {
+            version = buildProperties.getVersion();
+        }
         return new ApiInfoBuilder()
                 .title(swaggerProperties.getTitle())
                 .description(swaggerProperties.getDescription())
                 .contact(new Contact("", "", "caiqizhe@gmail.com"))
-                .version("1.0.0")
+                .version(version)
                 .build();
     }
 
