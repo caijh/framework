31731935f43f0e6af6d184ff2d1f2608b9cf4c13
caijh <caiqizhe@gmail.com>
2020-12-20 10:18:59 +0800

    refactor: rename

--- framework-utils/src/main/java/com/github/caijh/framework/core/exception/YamlParseException.java
+++ framework-utils/src/main/java/com/github/caijh/framework/core/exception/YamlParseException.java
@@ -2,8 +2,8 @@ package com.github.caijh.framework.core.exception;
 
 public class YamlParseException extends RuntimeException {
 
-    public YamlParseException(Exception e) {
-        super(e);
+    public YamlParseException(Throwable cause) {
+        super(cause);
     }
 
 }
