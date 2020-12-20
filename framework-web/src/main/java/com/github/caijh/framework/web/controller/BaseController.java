3160d13b6d1fbaa3e095966f96e63cf0782f1a3b
caijh <caiqizhe@gmail.com>
2020-12-20 10:38:09 +0800

    feat: Add initBinder to BaseController

--- framework-web/src/main/java/com/github/caijh/framework/web/controller/BaseController.java
+++ framework-web/src/main/java/com/github/caijh/framework/web/controller/BaseController.java
@@ -1,10 +1,23 @@
 package com.github.caijh.framework.web.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 
 public abstract class BaseController {
 
     protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        //使用spring自带的CustomDateEditor
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
 
 }
