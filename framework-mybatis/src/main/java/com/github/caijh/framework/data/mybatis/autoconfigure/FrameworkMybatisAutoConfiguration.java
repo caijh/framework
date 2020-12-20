13f243efbc6a60b5975f91a7845ecf66012e813a
caijh <caiqizhe@gmail.com>
2020-12-20 20:37:28 +0800

    feat: Add Mybatis-Plus

--- framework-mybatis/src/main/java/com/github/caijh/framework/data/mybatis/autoconfigure/FrameworkMybatisAutoConfiguration.java
+++ framework-mybatis/src/main/java/com/github/caijh/framework/data/mybatis/autoconfigure/FrameworkMybatisAutoConfiguration.java
@@ -1,8 +1,21 @@
 package com.github.caijh.framework.data.mybatis.autoconfigure;
 
+import com.baomidou.mybatisplus.core.mapper.Mapper;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@MapperScan(markerInterface = Mapper.class)
 public class FrameworkMybatisAutoConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
 
 }
