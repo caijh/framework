632a5a2ea259b68ed4868eaecfcb1677f01e5dfa
caijh <caiqizhe@gmail.com>
2020-12-21 16:29:54 +0800

    fix: @Primary to solve multi bean

--- framework-data-redis/src/main/java/com/github/caijh/framework/data/redis/autoconfigure/FrameworkRedisAutoConfiguration.java
+++ framework-data-redis/src/main/java/com/github/caijh/framework/data/redis/autoconfigure/FrameworkRedisAutoConfiguration.java
@@ -2,10 +2,12 @@ package com.github.caijh.framework.data.redis.autoconfigure;
 
 import com.github.caijh.framework.data.redis.Redis;
 import com.github.caijh.framework.data.redis.serializer.ProtobufSerializer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -15,7 +17,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableConfigurationProperties(RedisProperties.class)
 public class FrameworkRedisAutoConfiguration {
 
+    @Primary
     @Bean
+    @ConditionalOnMissingBean
     public RedisTemplate<String, Object> stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
@@ -26,7 +30,6 @@ public class FrameworkRedisAutoConfiguration {
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
-
 
     @Bean
     public Redis redis(RedisTemplate<String, Object> stringRedisTemplate) {
