1addc2c1d24d59becee443cf83770ae453ac1281
caijh <caiqizhe@gmail.com>
2020-12-27 12:30:56 +0800

    feat: Add querydsl

--- framework-data-jpa/src/main/java/com/github/caijh/framework/data/jpa/BaseRepository.java
+++ framework-data-jpa/src/main/java/com/github/caijh/framework/data/jpa/BaseRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
+/**
+ * 基础Repository
+ *
+ * @param <T> 实体.
+ * @param <I> 实体主键类型
+ */
 @NoRepositoryBean
 public interface BaseRepository<T, I> extends JpaRepository<T, I>, QuerydslPredicateExecutor<T> {
 }
