62d5c6d6c191679854740223d912e4b51c6d0fa0
caijh <caiqizhe@gmail.com>
2021-01-03 15:34:54 +0800

    feat: Add Constants.Date

---
+++ framework-utils/src/main/java/com/github/caijh/framework/util/constant/Constants.java
@@ -0,0 +1,25 @@
+package com.github.caijh.framework.util.constant;
+
+public class Constants {
+
+    private Constants() {
+
+    }
+
+    public static class Date {
+
+        public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+        public static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
+        public static final String YYYY_MM_DD_HH_MM = "yyyy-MM-dd HH:mm";
+        public static final String YYYYMMDD = "yyyyMMdd";
+        public static final String YYYY_MM = "yyyy-MM";
+        public static final String MM_DD = "MM-dd";
+        public static final String HHMMSS = "HH:mm:ss";
+
+        private Date() {
+
+        }
+
+    }
+
+}
