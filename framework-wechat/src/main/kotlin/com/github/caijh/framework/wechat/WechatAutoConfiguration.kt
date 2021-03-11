248935c12e0b2eabf8eb016d0b7253d1dfc575ab
caijunhui <caiqizhe@gmail.com>
2021-03-11 11:26:59 +0800

    feat: framework-wechat

---
+++ framework-wechat/src/main/kotlin/com/github/caijh/framework/wechat/WechatAutoConfiguration.kt
@@ -0,0 +1,8 @@
+package com.github.caijh.framework.wechat
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan
+open class WechatAutoConfiguration
---
+++ framework-wechat/src/main/kotlin/com/github/caijh/framework/wechat/env/StarterConfigFileEnvironmentPostProcessor.kt
@@ -0,0 +1,5 @@
+package com.github.caijh.framework.wechat.env
+
+import com.github.caijh.framework.core.processor.ConfigFileEnvironmentPostProcessor
+
+class StarterConfigFileEnvironmentPostProcessor : ConfigFileEnvironmentPostProcessor()
