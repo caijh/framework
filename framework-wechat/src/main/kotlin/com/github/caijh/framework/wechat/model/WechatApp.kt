0ddeff7ead7a8dc6dbde8797791ee73d35023f57
蔡俊辉 <caqizhe@gmail.com>
2021-03-13 20:58:49 +0800

    refactor: WechatApp defautl type unknown

--- framework-wechat/src/main/kotlin/com/github/caijh/framework/wechat/model/WechatApp.kt
+++ framework-wechat/src/main/kotlin/com/github/caijh/framework/wechat/model/WechatApp.kt
@@ -2,6 +2,4 @@ package com.github.caijh.framework.wechat.model
 
 import com.github.caijh.framework.wechat.enums.WechatType
 
-data class WechatApp(var appId: String, var secret: String) {
-    var type = WechatType.UNKNOWN
-}
+data class WechatApp(var appId: String, var secret: String, var type: WechatType = WechatType.UNKNOWN)
--- framework-wechat/src/test/java/com/github/caijh/framework/wechat/service/impl/WechatApisImplTest.kt
+++ framework-wechat/src/test/java/com/github/caijh/framework/wechat/service/impl/WechatApisImplTest.kt
@@ -1,5 +1,6 @@
 package com.github.caijh.framework.wechat.service.impl
 
+import com.github.caijh.framework.wechat.enums.WechatType
 import com.github.caijh.framework.wechat.model.WechatApp
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
@@ -7,7 +8,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class WechatApisImplTest {
-    private val wechatApp = WechatApp("wx3122a2651783fb93", "36e1aec13f873b5965a9f85e4e56c6c4")
+    private val wechatApp = WechatApp("wx3122a2651783fb93", "36e1aec13f873b5965a9f85e4e56c6c4", WechatType.MINI)
     private lateinit var wechatApisImpl: WechatApisImpl
 
     @BeforeEach
