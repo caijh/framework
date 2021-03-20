1ec45563a5e87e9e800e41c534d6d11856ca4aab
蔡俊辉 <caqizhe@gmail.com>
2021-03-20 10:30:10 +0800

    fix: WechatMiniLoginRespBody.kt

--- framework-wechat/src/main/java/com/github/caijh/framework/wechat/model/WechatMiniLoginRespBody.kt
+++ framework-wechat/src/main/java/com/github/caijh/framework/wechat/model/WechatMiniLoginRespBody.kt
@@ -1,7 +1,12 @@
 package com.github.caijh.framework.wechat.model
 
+import com.alibaba.fastjson.annotation.JSONField
+
 class WechatMiniLoginRespBody : WechatLoginRespBody() {
+    @JSONField(name = "session_key")
     val sessionKey: String? = null
-    val openId: String? = null
-    val unionId: String? = null
+
+    val openid: String? = null
+
+    val unionid: String? = null
 }
