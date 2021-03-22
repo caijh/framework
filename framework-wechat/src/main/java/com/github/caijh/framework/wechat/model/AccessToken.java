9965b44840ac3ec428e477868ff3fb5d7916cf33
caijunhui <caiqizhe@gmail.com>
2021-03-22 20:41:36 +0800

    chore: Add support Jackson

--- framework-wechat/src/main/java/com/github/caijh/framework/wechat/model/AccessToken.java
+++ framework-wechat/src/main/java/com/github/caijh/framework/wechat/model/AccessToken.java
@@ -1,15 +1,18 @@
 package com.github.caijh.framework.wechat.model;
 
 import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class AccessToken {
 
     @JSONField(name = "access_token")
+    @JsonProperty("access_token")
     private String token;
 
     @JSONField(name = "expires_in")
+    @JsonProperty("expires_in")
     private Integer expiresIn;
 
     public String token() {
