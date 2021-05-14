35d425d4faad633156b4ed96a04a6352dc00edf1
蔡俊辉 <caijunhui@pcitech.com>
2021-05-14 16:42:22 +0800

    fix: repositoryBaseClass error

--- framework-data-jpa/src/main/java/com/github/caijh/framework/data/FrameworkJpaAutoConfiguration.java
+++ framework-data-jpa/src/main/java/com/github/caijh/framework/data/FrameworkJpaAutoConfiguration.java
@@ -1,10 +1,7 @@
 package com.github.caijh.framework.data;
 
-import com.github.caijh.framework.data.jpa.BaseRepository;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(repositoryBaseClass = BaseRepository.class)
 public class FrameworkJpaAutoConfiguration {
 }
