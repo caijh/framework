06d53ce1f6358be2040e863237353ebba232ca92
蔡俊辉 <caqizhe@gmail.com>
2021-05-20 00:00:23 +0800

    feature/framework-config-apollo

---
+++ framework-demo/framework-demo-config-apollo/src/main/java/com/github/caijh/framework/demo/config/ConfigApolloApplication.java
@@ -0,0 +1,13 @@
+package com.github.caijh.framework.demo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApolloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApolloApplication.class, args);
+    }
+
+}
