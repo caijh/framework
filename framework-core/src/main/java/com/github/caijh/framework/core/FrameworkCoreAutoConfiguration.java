b46ac0a6bb9b976c4a1c416323767dfaa2c66455
蔡俊辉 <caijunhui@pcitech.com>
2021-05-28 14:25:14 +0800

    feta: Add FrameworkCoreAutoConfiguration to support component scan

---
+++ framework-core/src/main/java/com/github/caijh/framework/core/FrameworkCoreAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.caijh.framework.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class FrameworkCoreAutoConfiguration {
+}
