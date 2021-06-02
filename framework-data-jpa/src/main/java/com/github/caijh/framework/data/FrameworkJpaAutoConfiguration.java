7d2b33bfab86556f363d7770fda0e4a17b30af2f
蔡俊辉 <caqizhe@gmail.com>
2021-06-02 22:11:42 +0800

    fix: delete ConfigFileEnvironmentPostProcessor

--- framework-data-jpa/src/main/java/com/github/caijh/framework/data/FrameworkJpaAutoConfiguration.java
+++ framework-data-jpa/src/main/java/com/github/caijh/framework/data/FrameworkJpaAutoConfiguration.java
@@ -1,8 +1,7 @@
 package com.github.caijh.framework.data;
 
-import com.github.caijh.framework.core.processor.ConfigFileEnvironmentPostProcessor;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class FrameworkJpaAutoConfiguration extends ConfigFileEnvironmentPostProcessor {
+public class FrameworkJpaAutoConfiguration {
 }
