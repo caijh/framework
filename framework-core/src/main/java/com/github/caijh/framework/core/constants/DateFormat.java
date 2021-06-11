e3f5a62ea822c1592523489130620668a54070a3
蔡俊辉 <caijunhui@pcitech.com>
2021-06-11 18:06:41 +0800

    feat: Add more format

--- framework-core/src/main/java/com/github/caijh/framework/core/constants/DateFormat.java
+++ framework-core/src/main/java/com/github/caijh/framework/core/constants/DateFormat.java
@@ -2,14 +2,18 @@ package com.github.caijh.framework.core.constants;
 
 public class DateFormat {
 
-    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
     public static final String YYYY_MM_DD_HH_MM_SS_SSS = "yyyy-MM-dd HH:mm:ss.SSS";
+    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
     public static final String YYYY_MM_DD_HH_MM = "yyyy-MM-dd HH:mm";
+    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+    public static final String YYYY_MM = "yyyy-MM";
     public static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
     public static final String YYYYMMDD = "yyyyMMdd";
-    public static final String YYYY_MM = "yyyy-MM";
+    public static final String YYYYMM = "yyyyMM";
+    public static final String YYYY = "yyyy";
     public static final String MM_DD = "MM-dd";
-    public static final String HHMMSS = "HH:mm:ss";
+    public static final String MMDD = "MMdd";
+    public static final String HH_MM_SS = "HH:mm:ss";
 
     private DateFormat() {
 
