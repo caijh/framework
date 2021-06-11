719b696ff65607ac023ff1e1eb2b475c2af464f3
蔡俊辉 <caqizhe@gmail.com>
2021-06-11 23:32:25 +0800

    refactor

--- framework-data-redis/src/main/java/com/github/caijh/framework/data/redis/support/UserSign.java
+++ framework-data-redis/src/main/java/com/github/caijh/framework/data/redis/support/UserSign.java
@@ -3,8 +3,10 @@ package com.github.caijh.framework.data.redis.support;
 import java.time.LocalDate;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class UserSign<T> {
 
     private T userId;
