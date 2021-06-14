62466d76a51829f1c56ca9acec953a36ce659adc
蔡俊辉 <caqizhe@gmail.com>
2021-06-14 10:08:25 +0800

    build: 引入spring-retry

--- framework-core/src/main/java/com/github/caijh/framework/core/FrameworkCoreAutoConfiguration.java
+++ framework-core/src/main/java/com/github/caijh/framework/core/FrameworkCoreAutoConfiguration.java
@@ -1,9 +1,28 @@
 package com.github.caijh.framework.core;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.TimeoutRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 @ComponentScan
+@EnableRetry
 public class FrameworkCoreAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(RetryTemplate.class)
+    public RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+        RetryPolicy retryPolicy = new TimeoutRetryPolicy();
+        retryTemplate.setRetryPolicy(retryPolicy);
+        retryTemplate.setBackOffPolicy(new FixedBackOffPolicy());
+        return retryTemplate;
+    }
+
 }
