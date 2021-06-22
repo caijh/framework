c5f0f96a7b2e8b50961c1bc99f6e542a43385b4f
蔡俊辉 <caqizhe@gmail.com>
2021-06-22 21:11:15 +0800

    fix: remove StarterConfigFileEnvironmentPostProcessor.kt

--- framework-wechat/src/main/kotlin/com/github/caijh/framework/wechat/env/StarterConfigFileEnvironmentPostProcessor.kt
+++
@@ -1,5 +0,0 @@
-package com.github.caijh.framework.wechat.env
-
-import com.github.caijh.framework.core.processor.ConfigFileEnvironmentPostProcessor
-
-class StarterConfigFileEnvironmentPostProcessor : ConfigFileEnvironmentPostProcessor()
