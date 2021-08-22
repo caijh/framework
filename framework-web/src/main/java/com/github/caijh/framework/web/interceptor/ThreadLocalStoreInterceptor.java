60ee18aa4b717f8bfa9c496fbb700f017351a5b5
caijh <caiqizhe@gmail.com>
2021-08-22 10:46:09 +0800

    Fix CheckStyle problems

--- framework-web/src/main/java/com/github/caijh/framework/web/interceptor/ThreadLocalStoreInterceptor.java
+++ framework-web/src/main/java/com/github/caijh/framework/web/interceptor/ThreadLocalStoreInterceptor.java
@@ -10,7 +10,8 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 public class ThreadLocalStoreInterceptor extends HandlerInterceptorAdapter {
 
     @Override
-    public void afterCompletion(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler, Exception ex) throws Exception {
+    public void afterCompletion(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler, Exception ex)
+            throws Exception {
         super.afterCompletion(request, response, handler, ex);
         ThreadLocalStore.reset();
     }
