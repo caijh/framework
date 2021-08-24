a955c9eaf694693bba326521340616a4e243979e
caijh <caiqizhe@gmail.com>
2021-08-24 19:56:34 +0800

    Fixed @Service is also make as Repository

--- framework-data-jpa/src/main/java/com/github/caijh/framework/data/FrameworkJpaAutoConfiguration.java
+++ framework-data-jpa/src/main/java/com/github/caijh/framework/data/FrameworkJpaAutoConfiguration.java
@@ -1,7 +1,15 @@
 package com.github.caijh.framework.data;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Service;
 
 @Configuration
+@EnableJpaRepositories(
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Service.class)
+        })
 public class FrameworkJpaAutoConfiguration {
 }
