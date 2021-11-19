6af3a2eb6ff5e1e6ca5b8b1e3a1974a38c68d129
蔡俊辉(10039330) <caijh@glodon.com>
2021-11-19 11:49:08 +0800

    chore: commented

--- framework-model/src/main/java/com/github/caijh/framework/core/enums/CommonStatus.java
+++ framework-model/src/main/java/com/github/caijh/framework/core/enums/CommonStatus.java
@@ -1,10 +1,16 @@
 package com.github.caijh.framework.core.enums;
 
 /**
- * 共用状态.
+ * 通用状态.
  */
 public enum CommonStatus implements IndexEnum {
+    /**
+     * 在用
+     */
     IN_USE(0),
+    /**
+     * 逻辑删除
+     */
     DELETED(1);
 
     private final Integer index;
