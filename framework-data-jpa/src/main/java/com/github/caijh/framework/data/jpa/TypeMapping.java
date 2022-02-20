f3ec582a07b742362ef3adf6814d3185042c146f
johnhuitsai <caiqizhe@gmail.com>
2022-02-20 13:51:33 +0800

    refactor

--- framework-data-jpa/src/main/java/com/github/caijh/framework/data/jpa/TypeMapping.java
+++ framework-data-jpa/src/main/java/com/github/caijh/framework/data/jpa/TypeMapping.java
@@ -2,7 +2,7 @@ package com.github.caijh.framework.data.jpa;
 
 public class TypeMapping {
 
-    public static final String TYPE_JSON = "json";
+    public static final String JSON = "json";
 
     private TypeMapping() {}
 
