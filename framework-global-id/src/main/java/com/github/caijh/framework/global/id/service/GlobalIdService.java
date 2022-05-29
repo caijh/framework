3d35dcb89dc492a3b794df69583886e6b1fb7a72
johnhuitsai <caiqizhe@gmail.com>
2022-05-29 20:57:13 +0800

    refactor: move framework-wechat

--- framework-global-id/src/main/java/com/github/caijh/framework/global/id/service/GlobalIdService.java
+++ framework-global-id/src/main/java/com/github/caijh/framework/global/id/service/GlobalIdService.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public interface GlobalIdService {
 
     default long generate(Long increase) {
-        long saltA = new Random(System.currentTimeMillis()).nextLong(0, 256);
-        long saltB = new Random(System.currentTimeMillis()).nextLong(0, 256);
+        long saltA = new Random(System.currentTimeMillis()).nextLong(0L, 256L);
+        long saltB = new Random(System.currentTimeMillis()).nextLong(0L, 256L);
         return increase << 16 | saltA << 8 | saltB;
     }
 
