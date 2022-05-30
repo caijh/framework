da4b24891bca97743935aaeb8ab242e860bcf4ef
蔡俊辉(10039330) <caijh@glodon.com>
2022-05-30 11:05:00 +0800

    fix: article page

--- framework-data-jpa/src/main/java/com/github/caijh/framework/data/utils/PageRequestUtils.java
+++ framework-data-jpa/src/main/java/com/github/caijh/framework/data/utils/PageRequestUtils.java
@@ -1,7 +1,6 @@
 package com.github.caijh.framework.data.utils;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 
 import com.github.caijh.framework.core.model.PageReqBody;
@@ -16,7 +15,7 @@ public class PageRequestUtils {
     public static PageRequest newPageRequest(@Nonnull PageReqBody pageReqBody) {
         List<Sort.Order> orders = pageReqBody.getSorts().stream()
                                              .map(e -> new Sort.Order(Sort.Direction.fromString(e.getOrder()), e.getColumn()))
-                                             .collect(Collectors.toList());
+                                             .toList();
         return PageRequest.of(pageReqBody.getPageNo(), pageReqBody.getPageSize(), Sort.by(orders));
     }
 
