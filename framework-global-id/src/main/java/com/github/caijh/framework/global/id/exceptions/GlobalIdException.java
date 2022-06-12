bb24f8f9050423856a97e090ce195a0d60c6c68e
johnhuitsai <caiqizhe@gmail.com>
2022-06-12 18:46:17 +0800

    refactor: ...

--- framework-global-id/src/main/java/com/github/caijh/framework/global/id/exceptions/GlobalIdException.java
+++
@@ -1,9 +0,0 @@
-package com.github.caijh.framework.global.id.exceptions;
-
-public class GlobalIdException extends RuntimeException {
-
-    public GlobalIdException(Throwable cause) {
-        super(cause);
-    }
-
-}
