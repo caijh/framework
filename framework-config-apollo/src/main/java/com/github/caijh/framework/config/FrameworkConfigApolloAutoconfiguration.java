8edb6855ae0f98785b530a2d3b6f8fa22937877f
johnhuitsai <caiqizhe@gmail.com>
2022-11-19 23:04:28 +0800

    refactor: remove framework-config-apollo

--- framework-config-apollo/src/main/java/com/github/caijh/framework/config/FrameworkConfigApolloAutoconfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.github.caijh.framework.config;
-
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableApolloConfig
-public class FrameworkConfigApolloAutoconfiguration {
-}
