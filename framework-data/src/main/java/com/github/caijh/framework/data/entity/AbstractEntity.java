3ffb8665c8a3f80fe496409ecb5299bb12c0b906
johnhuitsai <caiqizhe@gmail.com>
2022-12-04 11:40:35 +0800

    feat: update hibernate version

--- framework-data/src/main/java/com/github/caijh/framework/data/entity/AbstractEntity.java
+++ framework-data/src/main/java/com/github/caijh/framework/data/entity/AbstractEntity.java
@@ -1,27 +1,14 @@
 package com.github.caijh.framework.data.entity;
 
 import java.io.Serializable;
-import javax.persistence.MappedSuperclass;
 
-import com.vladmihalcea.hibernate.type.array.IntArrayType;
-import com.vladmihalcea.hibernate.type.array.StringArrayType;
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
-import com.vladmihalcea.hibernate.type.json.JsonNodeBinaryType;
-import com.vladmihalcea.hibernate.type.json.JsonNodeStringType;
-import com.vladmihalcea.hibernate.type.json.JsonStringType;
-import org.hibernate.annotations.TypeDef;
+import jakarta.persistence.MappedSuperclass;
 
 /**
  * 抽象基础实体类, AbstractEntity can not be changed to interface.
  *
  * @param <T> 实体类id类型
  */
-@TypeDef(name = "string-array", typeClass = StringArrayType.class)
-@TypeDef(name = "int-array", typeClass = IntArrayType.class)
-@TypeDef(name = "json", typeClass = JsonStringType.class)
-@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
-@TypeDef(name = "json-node", typeClass = JsonNodeStringType.class)
-@TypeDef(name = "jsonb-node", typeClass = JsonNodeBinaryType.class)
 @MappedSuperclass
 public abstract class AbstractEntity<T extends Serializable> implements PersistentObject<T> {
 
--- framework-data/src/main/java/com/github/caijh/framework/data/jpa/TypeMapping.java
+++
@@ -1,9 +0,0 @@
-package com.github.caijh.framework.data.jpa;
-
-public class TypeMapping {
-
-    public static final String JSON = "json";
-
-    private TypeMapping() {}
-
-}
