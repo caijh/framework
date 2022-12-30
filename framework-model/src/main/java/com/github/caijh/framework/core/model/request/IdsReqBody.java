bce107dfbfee01b9beb20e7747d8dbe53b052af6
蔡俊辉(10039330) <caijh@glodon.com>
2022-12-30 16:58:39 +0800

    feat: IdsReqBody T extends Serializable

--- framework-model/src/main/java/com/github/caijh/framework/core/model/request/IdsReqBody.java
+++ framework-model/src/main/java/com/github/caijh/framework/core/model/request/IdsReqBody.java
@@ -1,11 +1,12 @@
 package com.github.caijh.framework.core.model.request;
 
+import java.io.Serializable;
 import java.util.List;
 
 import lombok.Data;
 
 @Data
-public class IdsReqBody<T> implements ReqBody {
+public class IdsReqBody<T extends Serializable> implements ReqBody {
 
     private List<T> ids;
 
