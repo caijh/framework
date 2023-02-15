006f70abdf7308633df4bc352984f1ced0b890d6
johnhuitsai <caiqizhe@gmail.com>
2023-02-15 22:17:15 +0800

    feat(framework-config-consul): init framework-config-consul

---
+++ framework-config-consul/src/main/java/com/github/caijh/framework/config/FrameworkConfigConsulAutoconfiguration.java
@@ -0,0 +1,9 @@
+package com.github.caijh.framework.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@AutoConfiguration
+@EnableDiscoveryClient
+public class FrameworkConfigConsulAutoconfiguration {
+}
