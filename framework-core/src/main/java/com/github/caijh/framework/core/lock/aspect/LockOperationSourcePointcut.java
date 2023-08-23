abc02948129903c001dbc052d71ea6db657bca8a
蔡俊辉(10039330) <caijh@glodon.com>
2023-08-23 19:09:37 +0800

    feat(framework-core): Locking

--- framework-core/src/main/java/com/github/caijh/framework/core/lock/aspect/LockOperationSourcePointcut.java
+++ framework-core/src/main/java/com/github/caijh/framework/core/lock/aspect/LockOperationSourcePointcut.java
@@ -17,11 +17,23 @@ public class LockOperationSourcePointcut extends StaticMethodMatcherPointcut imp
         setClassFilter(new LockOperationSourceClassFilter());
     }
 
+    /**
+     * Returns true if the provided method matches the lock operation source, false otherwise.
+     *
+     * @param method      the method to check for a lock operation
+     * @param targetClass the target class of the method
+     * @return true if the method matches the lock operation source, false otherwise
+     */
     @Override
     public boolean matches(@NonNull Method method, @NonNull Class<?> targetClass) {
         return (lockOperationSource == null || lockOperationSource.getLockOperation(method, targetClass) != null);
     }
 
+    /**
+     * Sets the lock operation source.
+     *
+     * @param lockOperationSource the lock operation source to be set
+     */
     public void setLockOperationSource(@Nullable LockOperationSource lockOperationSource) {
         this.lockOperationSource = lockOperationSource;
     }
