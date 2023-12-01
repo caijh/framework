f89d3fb1ab174888d36741b0d5e1bb7caaa41bea
johnhuitsai <caiqizhe@gmail.com>
2023-12-01 22:46:16 +0800

    chore: Java doc

--- framework-web/src/main/java/com/github/caijh/framework/web/controller/BaseController.java
+++ framework-web/src/main/java/com/github/caijh/framework/web/controller/BaseController.java
@@ -10,13 +10,21 @@ import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 
+/**
+ * 基础控制器类.
+ */
 public abstract class BaseController {
 
     protected Logger logger = LoggerUtils.getLogger(this.getClass());
 
+    /**
+     * 初始化Binder
+     *
+     * @param binder 编码器
+     */
     @InitBinder
     public void initBinder(WebDataBinder binder) {
-        //使用spring自带的CustomDateEditor
+        // 使用spring自带的CustomDateEditor
         SimpleDateFormat dateFormat = new SimpleDateFormat(DateFormat.YYYY_MM_DD_HH_MM_SS);
         binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
     }
