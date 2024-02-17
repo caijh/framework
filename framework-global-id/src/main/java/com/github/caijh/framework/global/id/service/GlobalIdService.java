b57162a6b9266a11e68d64a71f79a4a0c28933b3
caijunhui <caiqizhe@gmail.com>
2024-02-17 23:27:34 +0800

    Replace Random with SecureRandom in GlobalIdService.

--- framework-global-id/src/main/java/com/github/caijh/framework/global/id/service/GlobalIdService.java
+++ framework-global-id/src/main/java/com/github/caijh/framework/global/id/service/GlobalIdService.java
@@ -1,12 +1,12 @@
 package com.github.caijh.framework.global.id.service;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public interface GlobalIdService {
 
     default long generate(Long increase) {
-        long saltA = new Random(System.currentTimeMillis()).nextLong(0L, 256L);
-        long saltB = new Random(System.currentTimeMillis()).nextLong(0L, 256L);
+        long saltA = new SecureRandom().nextLong(0L, 256L);
+        long saltB = new SecureRandom().nextLong(0L, 256L);
         return increase << 16 | saltA << 8 | saltB;
     }
 
