b379d04c3625a198b45853d51296fc4387fce3ae
蔡俊辉(10039330) <caijh@glodon.com>
2024-03-08 16:20:30 +0800

    Updated ServiceException class: corrected class description, removed unnecessary private constructors, and improved code consistency.

--- framework-core/src/main/java/com/github/caijh/framework/core/exception/ServiceException.java
+++ framework-core/src/main/java/com/github/caijh/framework/core/exception/ServiceException.java
@@ -1,28 +1,24 @@
 package com.github.caijh.framework.core.exception;
 
 /**
- * service base exception.
+ * Service base exception.
  */
 public abstract class ServiceException extends LocalizedException {
 
-    protected ServiceException(String code, Object[] params) {
+    public ServiceException(String code, Object[] params) {
         super(code, params);
     }
 
-    protected ServiceException(String code, Object[] params, Throwable cause) {
+    public ServiceException(String code, Object[] params, Throwable cause) {
         super(code, params, cause);
     }
 
-    protected ServiceException(String message) {
+    public ServiceException(String message) {
         super(message);
     }
 
-    protected ServiceException(String message, Throwable cause) {
+    public ServiceException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    protected ServiceException(Throwable cause) {
-        super(null, null, cause);
     }
 
 }
