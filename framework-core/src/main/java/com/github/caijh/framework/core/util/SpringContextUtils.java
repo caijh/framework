850e7c11584298963fe5d903b69455d36894a4e1
蔡俊辉(10039330) <caijh@glodon.com>
2024-04-01 09:29:49 +0800

    Use lombok.Getter instead of getter.

--- framework-core/src/main/java/com/github/caijh/framework/core/util/SpringContextUtils.java
+++ framework-core/src/main/java/com/github/caijh/framework/core/util/SpringContextUtils.java
@@ -1,5 +1,6 @@
 package com.github.caijh.framework.core.util;
 
+import lombok.Getter;
 import org.springframework.context.ApplicationContext;
 
 /**
@@ -7,13 +8,10 @@ import org.springframework.context.ApplicationContext;
  */
 public class SpringContextUtils {
 
+    @Getter
     private static ApplicationContext applicationContext;
 
     private SpringContextUtils() {}
-
-    public static ApplicationContext getApplicationContext() {
-        return applicationContext;
-    }
 
     public static void setApplicationContext(ApplicationContext applicationContext) {
         SpringContextUtils.applicationContext = applicationContext;
